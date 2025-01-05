291914292703e533f347423ac200f934f026cccf
AyuzZ <yout27542@gmail.com>
2025-01-05 11:35:27 +0545

    Almost all features completed. Feature 2 and 3 fully needed to be done. Need to use Radzen Components. Need to seggregate files more properly.

---
+++ CredWatch/Models/DashboardDisplayTransactionModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredWatch.Models
+{
+    public class DashboardDisplayTransactionModel
+    {
+        public DashboardDisplayTransactionModel()
+        {
+        }
+
+        public int TransactionId { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public string CategoryId { get; set; }
+
+        public string TagId { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DashboardDisplayTransactionModel(int transactionId, string title, decimal amount, string categoryId, DateTime createdDate, string tagId)
+        {
+            TransactionId = transactionId;
+            Title = title;
+            Amount = amount;
+            CategoryId = categoryId;
+            CreatedDate = createdDate;
+            TagId = tagId;
+        }
+    }
+}
